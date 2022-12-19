c84c6003068d6fe35cf0707a1b6704d1ab20e10e
reshmyunni <reshmyunni1778@gmail.com>
2022-12-19 16:37:03 +0530

    latest code

--- TestUIPkg/ImageViewController.swift
+++ TestUIPkg/ImageViewController.swift
@@ -7,15 +7,15 @@
 
 import UIKit
 //import TestPackage
-import PMSuperButton
+//#import PMSuperButton
 
 public class ImageViewController: UIViewController {
 
     @IBOutlet weak var nextBtn: UIButton!
-    @IBOutlet weak var apiBtn: PMSuperButton!
+    @IBOutlet weak var apiBtn: UIButton!
     
     @IBOutlet weak var imgView: UIImageView!
-   
+
     public override func viewDidLoad() {
         setTheme()
     }
@@ -31,7 +31,7 @@ public class ImageViewController: UIViewController {
     }
     
     @IBAction func apiCallBtnTapped(_ sender: Any) {
-        apiBtn.showLoader(userInteraction: true)
+//        apiBtn.showLoader(userInteraction: true)
         APIRequest.sharedInstance.apiCall(request:"", parameters:[:], success: onAPISuccess, failure: onAPIFailed)
     }
     
